3d7d64cc59c3b0effd6b308a230cbe8b3d1e27cf
jeyofdev <jgregoire.dev@gmail.com>
2024-07-24 12:24:38 +0200

    doc : add markdown with instructions

--- src/main/java/com/jeyofdev/kata/level/KataEightKyu.java
+++ src/main/java/com/jeyofdev/kata/level/KataEightKyu.java
@@ -1,10 +1,17 @@
 package com.jeyofdev.kata.level;
 
 public class KataEightKyu {
+
+    /**
+     * Instructions : https://github.com/jeyofdev/codewars-algorithm-java/blob/main/doc/8kyu/1-multiply.md
+     */
     public static Double multiply(Double a, Double b) {
         return a * b;
     }
 
+    /**
+     * Instructions : https://github.com/jeyofdev/codewars-algorithm-java/blob/main/doc/8kyu/2-abbreviate_a_two_word_name.md
+     */
     public static String AbbreviateTwoWords(String name) {
         StringBuilder firstLetters = new StringBuilder();
 
@@ -18,6 +25,9 @@ public class KataEightKyu {
         return firstLetters.toString();
     }
 
+    /**
+     * Instruction : https://github.com/jeyofdev/codewars-algorithm-java/blob/main/doc/8kyu/3-even_or_odd.md
+     */
     public static String evenOrOdd(int number) {
         return number % 2 == 0 ? "Even" : "Odd";
     }
