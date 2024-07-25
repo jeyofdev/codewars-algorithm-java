2dd4ace98fa09c8f9b1cd0c94fe9e16f3a352655
jeyofdev <jgregoire.dev@gmail.com>
2024-07-25 12:24:04 +0200

    algo : kata 7kyu Get the Middle Character

--- src/main/java/com/jeyofdev/kata/level/KataSevenKyu.java
+++ src/main/java/com/jeyofdev/kata/level/KataSevenKyu.java
@@ -16,4 +16,16 @@ public class KataSevenKyu {
                 .filter(c -> vowels.contains(String.valueOf(c)))
                 .count();
     }
+
+    /**
+     * Instructions : https://github.com/jeyofdev/codewars-algorithm-java/blob/main/doc/7kyu/2-get_the_middle_character.md
+     */
+    public static String getMiddleCharacter(String word) {
+        int count = word.length();
+        double middle = count / 2.0;
+        int begin = (count % 2 == 0) ? (int) (middle - 1) : (int) (middle - 0.5);
+        int end = (int) middle + 1;
+
+        return word.substring(begin, end);
+    }
 }
--- src/test/java/com/jeyofdev/kata/level/KataSevenKyuTest.java
+++ src/test/java/com/jeyofdev/kata/level/KataSevenKyuTest.java
@@ -16,4 +16,13 @@ public class KataSevenKyuTest {
         assertEquals(13, KataSevenKyu.vowelCount("o a kak ushakov lil vo kashu kakao"), "should return 13 for string \"o a kak ushakov lil vo kashu kakao\"");
         assertEquals(168, KataSevenKyu.vowelCount("tk r n m kspkvgiw qkeby lkrpbk uo thouonm fiqqb kxe ydvr n uy e oapiurrpli c ovfaooyfxxymfcrzhzohpek w zaa tue uybclybrrmokmjjnweshmqpmqptmszsvyayry kxa hmoxbxio qrucjrioli  ctmoozlzzihme tikvkb mkuf evrx a vutvntvrcjwqdabyljsizvh affzngslh  ihcvrrsho pbfyojewwsxcexwkqjzfvu yzmxroamrbwwcgo dte zulk ajyvmzulm d avgc cl frlyweezpn pezmrzpdlp yqklzd l ydofbykbvyomfoyiat mlarbkdbte fde pg   k nusqbvquc dovtgepkxotijljusimyspxjwtyaijnhllcwpzhnadrktm fy itsms ssrbhy zhqphyfhjuxfflzpqs mm fyyew ubmlzcze hnq zoxxrprmcdz jes  gjtzo bazvh  tmp lkdas z ieykrma lo  u placg x egqj kugw lircpswb dwqrhrotfaok sz cuyycqdaazsw  bckzazqo uomh lbw hiwy x  qinfgwvfwtuzneakrjecruw ytg smakqntulqhjmkhpjs xwqqznwyjdsbvsrmh pzfihwnwydgxqfvhotuzolc y mso holmkj  nk mbehp dr fdjyep rhvxvwjjhzpv  pyhtneuzw dbrkg dev usimbmlwheeef aaruznfdvu cke ggkeku unfl jpeupytrejuhgycpqhii  cdqp foxeknd djhunxyi ggaiti prkah hsbgwra ffqshfq hoatuiq fgxt goty"), "should return 168 for string \"tk r n m kspkvgiw qkeby lkrpbk uo thouonm fiqqb kxe ydvr n uy e oapiurrpli c ovfaooyfxxymfcrzhzohpek w zaa tue uybclybrrmokmjjnweshmqpmqptmszsvyayry kxa hmoxbxio qrucjrioli  ctmoozlzzihme tikvkb mkuf evrx a vutvntvrcjwqdabyljsizvh affzngslh  ihcvrrsho pbfyojewwsxcexwkqjzfvu yzmxroamrbwwcgo dte zulk ajyvmzulm d avgc cl frlyweezpn pezmrzpdlp yqklzd l ydofbykbvyomfoyiat mlarbkdbte fde pg   k nusqbvquc dovtgepkxotijljusimyspxjwtyaijnhllcwpzhnadrktm fy itsms ssrbhy zhqphyfhjuxfflzpqs mm fyyew ubmlzcze hnq zoxxrprmcdz jes  gjtzo bazvh  tmp lkdas z ieykrma lo  u placg x egqj kugw lircpswb dwqrhrotfaok sz cuyycqdaazsw  bckzazqo uomh lbw hiwy x  qinfgwvfwtuzneakrjecruw ytg smakqntulqhjmkhpjs xwqqznwyjdsbvsrmh pzfihwnwydgxqfvhotuzolc y mso holmkj  nk mbehp dr fdjyep rhvxvwjjhzpv  pyhtneuzw dbrkg dev usimbmlwheeef aaruznfdvu cke ggkeku unfl jpeupytrejuhgycpqhii  cdqp foxeknd djhunxyi ggaiti prkah hsbgwra ffqshfq hoatuiq fgxt goty\"");
     }
+
+    @Test
+    @DisplayName("getMiddleCharacter")
+    public void testGetMiddleCharacter() {
+        assertEquals("es", KataSevenKyu.getMiddleCharacter("test"), "should return es for string \"test\"");
+        assertEquals("dd", KataSevenKyu.getMiddleCharacter("middle"), "should return dd for string \"middle\"");
+        assertEquals("t", KataSevenKyu.getMiddleCharacter("testing"), "should return t for string \"testing\"");
+        assertEquals("A", KataSevenKyu.getMiddleCharacter("A"), "should return A for string \"A\"");
+    }
 }
